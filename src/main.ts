4f24f6c439ce9e8aa576e297303796ea21466d28
BARETTE <zyrtec06200@live.fr>
2022-05-12 15:30:02 +0200

    First commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+"use strict";
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+const express = require('express');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 
+const httpsOptions = {
+	key: fs.readFileSync('./secrets/private-key.pem'),
+	cert: fs.readFileSync('./secrets/public-certificate.pem'),
+};
+
+const server = express();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(
+		AppModule,
+		new ExpressAdapter(server),
+	);
+	await app.init();
 }
 bootstrap();
+
+http.createServer(server).listen(3000);
+https.createServer(httpsOptions, server).listen(443);

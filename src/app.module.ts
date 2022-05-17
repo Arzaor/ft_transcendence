8f6f5267d9b3ebef9a64a800cce7e12292d8c5f9
jeremybarette <jeremy.barette@outlook.fr>
2022-05-17 22:18:18 +0200

    Add confugration dbb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './user/user.entity';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+		"type": "postgres",
+		"host": "db",
+		"port": 5432,
+		"username": "postgres",
+		"password": "root",
+		"database": "test_db",
+		"entities": [User],
+		"synchronize": true,
+	}), 
+	UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
